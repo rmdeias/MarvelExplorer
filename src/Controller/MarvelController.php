a2434988c1ba5d6a7c3ca73186e31c8f46b98e0d
rdeias <rdeias@mediastay.com>
2025-09-14 14:22:00 +0200

    feat(controller) : add new comics route

--- src/Controller/MarvelController.php
+++ src/Controller/MarvelController.php
@@ -12,7 +12,14 @@ final class MarvelController extends AbstractController
     #[Route('/marvel/characters', name: 'marvel_characters')]
     public function characters(MarvelApiService $marvelApi): JsonResponse
     {
-        $data = $marvelApi->getCharacters(5);
+        $data = $marvelApi->getCharacters(2, 900);
+        return $this->json($data);
+    }
+
+    #[Route('/marvel/comics', name: 'marvel_comics')]
+    public function comics(MarvelApiService $marvelApi): JsonResponse
+    {
+        $data = $marvelApi->getComics(2, 9);
         return $this->json($data);
     }
 }
