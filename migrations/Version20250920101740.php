306dd4b8664193adce49aa49b538eef82456d41d
rdeias <rdeias@mediastay.com>
2025-09-21 19:32:58 +0200

    feat(migration): add nullable fields

---
+++ migrations/Version20250920101740.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250920101740 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE comic CHANGE marvel_id_serie marvel_id_serie INT DEFAULT NULL, CHANGE marvel_ids_character marvel_ids_character JSON DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `comic` CHANGE marvel_id_serie marvel_id_serie INT NOT NULL, CHANGE marvel_ids_character marvel_ids_character JSON NOT NULL');
+    }
+}
