7e0b3bb41ad2c21680b68532d099e491190a9c27
rdeias <rdeias@mediastay.com>
2025-09-23 11:14:37 +0200

    chore(controller): clean files

--- src/Controller/MarvelController.php
+++ src/Controller/MarvelController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Service\MarvelApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MarvelController extends AbstractController
